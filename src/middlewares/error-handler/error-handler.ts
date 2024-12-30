36c12b0c84a5ac8baf3513d26de5280b07b08569
bacallaV <valdohalo3@hotmail.com>
2024-12-30 00:47:34 -0600

    refactor: error handler middleware

--- src/middlewares/error-handler/error-handler.ts
+++ src/middlewares/error-handler/error-handler.ts
@@ -1,12 +1,15 @@
 import { Request, Response, NextFunction } from 'express'
 import { HttpError } from './http-error'
 
-export function handleError (err: HttpError, _req: Request, res: Response, _next: NextFunction): void {
-  res.status(err.statusCode).json({
+export function handleError (err: HttpError | undefined, _req: Request, res: Response, _next: NextFunction): void {
+  res.status(err?.statusCode ?? 500).json({
     message: 'Ocurrió un error',
-    error: {
-      name: err.name,
-      message: err.description
-    }
+    error: (err != null)
+      ? {
+          name: err.name,
+          message: err.description,
+          errors: err?.errors ?? {}
+        }
+      : null
   })
 }
--- src/middlewares/error-handler/http-error.ts
+++ src/middlewares/error-handler/http-error.ts
@@ -2,12 +2,14 @@ export class HttpError extends Error {
   public statusCode: HttpStatusCode
   public name: string
   public description: string
+  public errors: Record<string, any>
 
-  constructor (statusCode: HttpStatusCode, name: string, description: string) {
+  constructor (statusCode: HttpStatusCode, name: string, description: string, errors: Record<string, any> = {}) {
     super(description)
     this.statusCode = statusCode
     this.name = name
     this.description = description
+    this.errors = errors
   }
 }
 
